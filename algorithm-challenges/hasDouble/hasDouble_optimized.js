2b65e88f8ae04cad692071c523c3513d361f4b8a
Alec Down <alecdown@gmail.com>
2019-05-31 16:30:38 -0500

    updates hasDouble algorithm with arr instead of array as parma name

--- algorithm-challenges/hasDouble/hasDouble_optimized.js
+++ algorithm-challenges/hasDouble/hasDouble_optimized.js
@@ -1,16 +1,18 @@
+// THE CHALLENGE
+
 // In this assignment, the function should take an array of numbers and check whether its double exists in the array.
 
 // For example:
 // If the array contains the number 2 and the number 4, then it would return true. Otherwise, false.
 
-function includesDoubleWithHashmap(array) {
+function includesDoubleWithHashmap(arr) {
     let doubles = {};
 
     let i = 0;
 
-    while (i < array.length) {
-        doubles[array[i]] = 'this text does not matter';
-        if (doubles[(array[i] * 2)] || doubles[(array[i] / 2)]) {
+    while (i < arr.length) {
+        doubles[arr[i]] = 'this text does not matter';
+        if (doubles[(arr[i] * 2)] || doubles[(arr[i] / 2)]) {
             return true;
         }
         // console.log('doubles: ' + JSON.stringify(doubles))
