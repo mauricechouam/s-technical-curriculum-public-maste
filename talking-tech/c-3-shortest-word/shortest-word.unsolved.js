c5ca61f94c7ebb108e24eed64594e5bef9ebceeb
Alec Down <alecdown@gmail.com>
2019-08-08 08:54:27 -0500

    adds c3 for talking  tech

---
+++ talking-tech/c-3-shortest-word/shortest-word.unsolved.js
@@ -0,0 +1,14 @@
+// Write a function that takes in a string of words and returns the length of the shortest word(s). Make the assumption that the string will never be empty, and you do not need to account for differing datatypes.
+
+
+function findShortest(str) {
+
+    // YOUR CODE HERE
+
+
+}
+
+// TEST CASES
+console.log(findShortest("For writers, a random sentence can help them get their creative juices flowing.")); // 1
+console.log(findShortest("Joe made the sugar cookies; Susan decorated them.")); // 3
+console.log(findShortest("We have never been to Asia, nor have we visited Africa.")); // 2
