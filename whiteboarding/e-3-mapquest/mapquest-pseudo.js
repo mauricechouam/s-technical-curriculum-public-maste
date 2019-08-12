cb9c80c079f518cc8aed4dd5d58369b22da99ff6
Alec Down <alecdown@gmail.com>
2019-08-12 08:36:16 -0500

    adds mapquest for whiteboarding e-3

---
+++ whiteboarding/e-3-mapquest/mapquest-pseudo.js
@@ -0,0 +1,24 @@
+// Write a function that takes in an array of numbers, and then returns a new array with each number doubled.
+
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+
+
+function mapquest(arr) {
+
+    // create an empty array for the new array with the doubled values
+
+    // loop through the input array
+
+    // for each value in arr, double it, and assign it to a new variable
+
+    // push this new value to the newArr
+
+    // once complete, return the newArry
+}
+
+
+// Test cases:
+console.log(mapquest([1, 2, 3])); //[2, 4, 6]
+console.log(mapquest([4, 3, 3, 3, 4])); //[8, 6, 6, 6, 8]
+console.log(mapquest([6, 6, 6, 6, 6, 6]); // [12, 12, 12, 12, 12, 12]
