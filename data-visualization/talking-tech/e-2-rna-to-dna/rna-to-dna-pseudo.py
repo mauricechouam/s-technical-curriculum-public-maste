df8e5c58926caf0aa3a6cb1ccfd422305cf8a6f0
Alec Down <alecdown@gmail.com>
2019-09-09 08:54:55 -0500

    adds e-1 and e-1 for data talking-tech

---
+++ data-visualization/talking-tech/e-2-rna-to-dna/rna-to-dna-pseudo.py
@@ -0,0 +1,25 @@
+# DNA (Deoxyribonucleic acid) is the primary information storage molecule. It is composed of four nucleic acid bases:
+# Guanine ('G')
+# Cytosine ('C')
+# Adenine ('A')
+# Thymine ('T').
+
+# RNA (Ribonucleic acid), is the primary messenger molecule in cells. RNA is slightly different from DNA  because its chemical structure and contains no Thymine ('T'). 
+
+# In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+# Create a function called `DNA_to_RNA` which translates a given DNA string into RNA.
+
+# For example:
+
+# `DNA_to_RNA("GCAT") returns ("GCAU")`
+
+# OPTION 1
+def DNA_to_RNA(dna):
+    # split the DNA string where there is a T
+
+    # rejoin the DNA portion of the string without the T, but add in a U instead
+
+    # return the value
+
+
