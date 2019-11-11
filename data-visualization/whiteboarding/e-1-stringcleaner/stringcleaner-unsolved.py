6dd4ef70cd7655e1f854e6464009d12b3c77ee81
Alec Down <alecdown@gmail.com>
2019-11-11 23:34:24 +0000

    Update stringcleaner-unsolved.py

--- data-visualization/whiteboarding/e-1-stringcleaner/stringcleaner-unsolved.py
+++ data-visualization/whiteboarding/e-1-stringcleaner/stringcleaner-unsolved.py
@@ -5,7 +5,7 @@
 # Your job is to write a function that takes these strings as inputs, and cleans out the numeric characters and return a string with special characters and spacing all intact.
 
 # For example:
-# string_clean('Thi3s looks65 awesom4e!') == 'This looks great!'
+# string_clean('Thi3s looks65 awesom4e!') == 'This looks awesome!'
 
 # Special characters include: ~#$%^&!@*():;"'.,?
 
